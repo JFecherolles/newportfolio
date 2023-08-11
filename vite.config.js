70bb7dfc0d6314fdf17e02121236ac63109c4b51
JFecherolles <fecherolles.jerome@outlook.fr>
2023-08-11 14:37:18 +0200

    modification chemin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: './',
@@ -11,7 +10,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
-    assetsInclude: ['**/*.png', '**/*.ico'],
+    assetsInclude: ['**/*.png', '**/*.ico', 'assets/images/**/*'],
   },
   base: './',
 })
